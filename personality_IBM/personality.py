020caec364d0f19fabd29ae05d390bb530e9d23b
saurabh sharma <37418187+Sourabh12sharma1@users.noreply.github.com>
2020-05-23 00:27:40 +0530

    Add files via upload

---
+++ personality_IBM/personality.py
@@ -0,0 +1,19 @@
+other = { 
+		
+		"1": ['Introversion','Intuition', 'Thinking','Judging'],		
+		'2':['Introversion', 'Intuition', 'Thinking', 'Perceiving'],
+		"3": ['Introversion', 'Intuition','Feeling','Perceiving'],
+		"4": ['Introversion', 'Intuition', 'Feeling','Judging'],
+		"6": ['Extroversion','Intuition', 'Feeling', 'Perceiving'],
+		"7": ['Extroversion','Intuition', 'Feeling', 'Judging'],
+		"8": ['Extroversion', 'Intuition', 'Thinking','Perceiving'],
+		"9": ['Extroversion', 'Intuition', 'Thinking', 'Judging' ],
+		"10": ['Introversion','Sensing', 'Feeling' , 'Judging'],
+		"11": ['Introversion','Sensing', 'Thinking', 'Perceving' ],
+		"12": ['Introversion','Sensing', 'Thinking', 'Judging' ],
+		"13": ['Extroversion','Sensing', 'Feeling', 'Perceving' ],
+		"14": ['Extroversion','Sensing', 'Feeling', 'Judging' ],
+		"15": ['Extroversion','Sensing', 'Thinking', 'Perceving'],
+		"16": ['Extroversion','Sensing', 'Thinking', 'Judging'],
+		
+		}
